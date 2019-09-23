3223738704a7c7980a8272d3324e92b7be73827c
mart1nu <sepo-martin.elb@khk.ee>
2019-09-23 12:31:20 +0300

    Ülesanne 2

---
+++ 23.09.2019/programm.php
@@ -0,0 +1,11 @@
+<?php
+$eesNimi = "Sepo-Martin";
+$pereNimi = "Elb";
+$vanus = "20";
+$pikkus = "1.80";
+$kaal = "60";
+
+echo $eesnimi." ".$perenimi."<br>";
+echo $vanus."<br>";
+echo $pikkus."<br>";
+echo $kaal."<br>";
