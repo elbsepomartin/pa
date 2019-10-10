0a05337ad06e4e9a489dc0c92e0c369decc5ee79
mart1nu <sepo-martin.elb@khk.ee>
2019-10-10 12:53:34 +0300

    php

---
+++ 10.10.2019/persons.php
@@ -0,0 +1,17 @@
+<?php
+
+// define variables
+$age = 75;
+echo 'Vanus = '.$age.'<br>';
+// 0 - 17 - laps
+if($age > 0 and $age < 18) {
+    echo 'Oled laps<br>';
+}
+// 18 - 65 - täiskasvanu
+if($age > 17 and $age < 66) {
+    echo 'Oled täiskasvanu<br>';
+}
+// 66 > - seenior
+if($age > 65) {
+    echo 'Oled seenior<br>';
+}
