cccfb9f2d564e8ae5d88c12ed86ccfa86feb6c66
mart1nu <sepo-martin.elb@khk.ee>
2019-10-10 14:18:41 +0300

    php

--- 10.10.2019/divide_by_zero_action.php
+++ 10.10.2019/divide_by_zero_action.php
@@ -1,1 +1,16 @@
 <?php
+
+$numberOne = (int)$_GET['$numberOne'];
+$numberTwo = (int)$_GET['$numberTwo'];
+var_dump($numberOne);
+var_dump($numberTwo);
+
+if(strlen($numberOne) > 0 or strlen($numberTwo) > 0) {
+    if($numberTwo == 0) {
+        echo 'Ei tohi jagada 0-ga';
+    }   else {
+        echo $numberOne / $numberTwo;
+    }
+}   else {
+    echo 'Sisesta väärtused';
+}
