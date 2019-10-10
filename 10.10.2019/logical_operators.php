5d036d8195ab247b78d2c8cc813f52e97c8bc314
mart1nu <sepo-martin.elb@khk.ee>
2019-10-10 12:39:24 +0300

    php

---
+++ 10.10.2019/logical_operators.php
@@ -0,0 +1,18 @@
+<?php
+
+// define variables
+$x = 2;
+$y = 3;
+// Logical operators
+var_dump($x == $y); // equal
+echo '<hr>';
+var_dump($x != $y); // not equal !-not
+echo '<hr>';
+var_dump($x > $y);
+echo '<hr>';
+var_dump($x < $y);
+echo '<hr>';
+var_dump($x >= $y);
+echo '<hr>';
+var_dump($x <= $y);
+echo '<hr>';
